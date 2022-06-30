bf1698eacc5640b90df03385a84361e2aaa57e13
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-30 10:05:50 -0500

    sql code created

--- database.sql
+++ database.sql
@@ -15,3 +15,14 @@ CREATE TABLE "item" (
     "image_url" VARCHAR (2083),
     "user_id" INT REFERENCES "user"
 );
+
+INSERT INTO "item" (description, image_url, user_id)
+VALUES 
+    ('a picture of mario', 'https://mario.nintendo.com/static/5173cb58b062e1ec02553be8ed1b8019/b2548/header-mario.png', 1);
+	('a picture of luigi', 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/super-mario-wii-u/d/d7/406px-Luigi_Artwork_-_Super_Mario_3D_World.png', 2),
+	('a picture of peach', 'https://static.wikia.nocookie.net/great-characters/images/b/b7/Uhuh.png/revision/latest?cb=20180624012247', 1);
+	
+INSERT INTO "user" ("username", "password")
+VALUES 
+('jeanlacosse', 'abc123')
+('andrewleck', 'abc123');
--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -4,6 +4,9 @@ function ShelfPage() {
   return (
     <div className="container">
       <h2>Shelf</h2>
+      {/* TODO
+      []add new item form from here to DB using img URLs 
+      [] delete item ont if same user logged in*/}
       <p>All of the available items can be seen here.</p>
     </div>
   );
